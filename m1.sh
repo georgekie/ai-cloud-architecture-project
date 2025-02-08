5bdf676b47277b514dfa532a9db72081e60a12ae
george kielty <george.kielty@gmail.com>
2025-02-08 17:27:31 +0100

    initial steps

---
+++ m1.sh
@@ -0,0 +1,7 @@
+echo "# ai-cloud-architecture-project" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M develop
+git remote add origin https://github.com/georgekie/ai-cloud-architecture-project.git
+git push -u origin develop
